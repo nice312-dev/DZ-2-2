698e80b323a93fa82dcf9687b200c1ad790c7243
nice312-dev <74597002+nice312-dev@users.noreply.github.com>
2020-12-15 12:50:18 +0500

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 35
-
-    println("Likes: $likes")
+    val likes = 23101;
+    val message = if (likes % 10 == 1 && likes % 100 !== 11) "человеку" else "людям"
+    println("Понравилось $likes $message \n")
 }
